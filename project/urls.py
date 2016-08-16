52ba342c5590e07ec0254ecbc038cf9ddf394d91
mac book air <fabio.garcia.sanchez@gmail.com>
2016-08-16 12:09:39 -0500

    To try using in debug mode on the server, I have disable the tool bar debug url in url.py

--- project/urls.py
+++ project/urls.py
@@ -31,10 +31,10 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ]
+    # import debug_toolbar
+    # urlpatterns += [
+    #     url(r'^__debug__/', include(debug_toolbar.urls)),
+    # ]
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
